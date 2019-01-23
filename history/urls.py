7277479444bae098bc509f149ae44b304657dc60
Rachel Daniel <rachdaniel88@gmail.com>
2019-01-23 15:52:22 -0600

    added form for adding albums

--- history/urls.py
+++ history/urls.py
@@ -7,6 +7,7 @@ app_name = "history"
 urlpatterns = [
     path('', views.artists, name='artists'),
     path('<int:artist_id>/', views.detail, name='detail'),
-    path('<int:artist_id>/add', views.add_song, name='add_song')
+    path('<int:artist_id>/addSong', views.add_song, name='add_song'),
+    path('<int:artist_id>/addAlbum', views.add_album, name='add_album')
 
 ]
--- history/views.py
+++ history/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from .models import Artist, Song, Album
+from .models import Artist, Song, Album, Genre
 
 
 def artists(request):
@@ -11,14 +11,20 @@ def artists(request):
 
 def detail(request, artist_id):
     artist = get_object_or_404(Artist, pk=artist_id)
-    song_list = Song.objects.filter(artist_id = artist_id)
-    context = {'song_list': song_list, 'artist': artist}
+    genre_list = Genre.objects.all()
+    context = {'genre_list': genre_list, 'artist': artist}
     return render(request, 'history/detail.html', context)
 
 def add_song(request, artist_id):
-    print("artist id:", artist_id)
     album = get_object_or_404(Album, pk=request.POST["album"])
     artist = get_object_or_404(Artist, pk=artist_id)
     new_song = Song(song_name=request.POST["song_name"], album=album, artist=artist)
     new_song.save()
+    return HttpResponseRedirect(reverse('history:detail', args=(artist.id, )))
+
+def add_album(request, artist_id):
+    genre = get_object_or_404(Genre, pk=request.POST["genre"])
+    artist = get_object_or_404(Artist, pk=artist_id)
+    new_album = Album(album_name=request.POST["album_name"], release_date=request.POST["release_date"], artist=artist, genre=genre)
+    new_album.save()
     return HttpResponseRedirect(reverse('history:detail', args=(artist.id, )))
